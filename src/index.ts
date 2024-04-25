95e0442037c64471719e66e41aa69b61f98b5ea3
Arto Hyvättinen <arto.hyvattinen@gmail.com>
2024-04-25 16:01:06 +0300

    AddonSession

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { KitsasAddon } from './kitsasAddon';
 export { AddonCall } from './addonCall';
 export { AddonOptions } from './addonOptions.dto';
+export { AddonSession } from './addonSession.interface';
 export * from 'kitsas-library';
